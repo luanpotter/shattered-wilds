14381563d83e2a6146ddcab6ae37a283ddbea75e
Luan Nico <luanpotter27@gmail.com>
2025-07-05 15:13:16 -0400

    Fix vite config for base href

--- packages/simulator/vite.config.ts
+++ packages/simulator/vite.config.ts
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/simulator/' : '/',
-})
+  base: mode === 'production' ? '/simulator/' : '/',
+}))
