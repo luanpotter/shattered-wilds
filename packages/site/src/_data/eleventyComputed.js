639aeedfedec703eb0433f47ba5f49aa451cd7f2
Luan Nico <luanpotter27@gmail.com>
2025-07-17 23:47:44 -0400

    Setup and add commons as a depedency to the eleventy website

--- packages/site/src/_data/eleventyComputed.js
+++ packages/site/src/_data/eleventyComputed.js
@@ -1,1 +1,9 @@
-export const eleventyComputed = {};
+import { StatType } from '@shattered-wilds/commons';
+
+const stats = Object.values(StatType).map(stat => ({
+	name: stat.name,
+}));
+
+export const eleventyComputed = {
+	stats,
+};
