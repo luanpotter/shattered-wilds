5e6e48399dc8973ec3c1ffb820e7adad8161bdf3
Luan Nico <luanpotter27@gmail.com>
2025-08-18 00:26:36 -0400

    Clarification of how Heroism Points are recovered

--- packages/commons/src/stats/resources.ts
+++ packages/commons/src/stats/resources.ts
@@ -50,7 +50,7 @@ export const RESOURCES: Record<Resource, ResourceDefinition> = {
 		shortName: 'HP',
 		description: `Represents a character's heroic potential in the narrative. Each character has a maximum number of Heroism Points equal to their [[Level]] value.
 
-They can be used to invoke [[Extra Die | Extras]] and [[Luck Die]], to call for [[Write History]], amongst other powerful actions. These are harder to recover, at one per long rest rate plus the DM can award Heroism Points (within your maximum) to reward good roleplaying or other out-of-the-box thinking (similar to the Inspiration mechanic in D&D 5e).`,
+They can be used to invoke [[Extra Die | Extras]] and [[Luck Die]], to call for [[Write History]], amongst other powerful actions. These are harder to recover, at one per [[Long Rest]] rate plus the DM can award Heroism Points (within your maximum) to reward good roleplaying or other out-of-the-box thinking (similar to the Inspiration mechanic in D&D 5e).`,
 		formula: F.level(),
 	}),
 	[Resource.VitalityPoint]: new ResourceDefinition({
