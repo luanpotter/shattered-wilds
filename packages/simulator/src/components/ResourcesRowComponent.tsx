11495e29efd5f3e452085e8f9ef4da82b14d3ff5
Luan Nico <luanpotter27@gmail.com>
2025-10-18 14:02:27 -0400

    feat: Remove old "Refill" button on simulator

--- packages/simulator/src/components/ResourcesRowComponent.tsx
+++ packages/simulator/src/components/ResourcesRowComponent.tsx
@@ -1,11 +1,9 @@
-import { CharacterSheet, CurrentResources, Resource } from '@shattered-wilds/commons';
+import { CharacterSheet, Resource } from '@shattered-wilds/commons';
 import React from 'react';
-import { FaBatteryFull } from 'react-icons/fa';
 
 import { useStore } from '../store';
 
 import { ResourceInputComponent } from './ResourceInputComponent';
-import { Button } from './shared/Button';
 
 export const ResourcesRowComponent: React.FC<{
 	variant: 'normal' | 'inline';
@@ -14,12 +12,6 @@ export const ResourcesRowComponent: React.FC<{
 	const character = useStore(state => state.characters.find(c => c.id === characterId))!;
 	const sheet = CharacterSheet.from(character.props);
 
-	const updateCharacterProp = useStore(state => state.updateCharacterProp);
-	const handleRefillPoints = () => {
-		Object.values(Resource).forEach(resource => {
-			updateCharacterProp(character, resource, CurrentResources.MAX_VALUE.toString());
-		});
-	};
 	return (
 		<>
 			{Object.values(Resource).map(resource => (
@@ -31,9 +23,6 @@ export const ResourcesRowComponent: React.FC<{
 					resource={resource}
 				/>
 			))}
-			<div style={{ display: 'flex', alignItems: 'end', marginBottom: '0.75rem' }}>
-				<Button variant={variant} title='Refill points' icon={FaBatteryFull} onClick={handleRefillPoints} />
-			</div>
 		</>
 	);
 };
