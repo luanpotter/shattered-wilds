dc4111db13ce33a7534aaa3d2baadbe1b478f7fc
Luan Nico <luanpotter27@gmail.com>
2026-01-11 16:45:30 -0500

    Add markdown table support to simulator; use pre-processor for markdown links and hook up custom tags on RichText

--- packages/simulator/src/components/shared/RichText.tsx
+++ packages/simulator/src/components/shared/RichText.tsx
@@ -1,35 +1,13 @@
+import { MarkdownPreProcessor } from '@shattered-wilds/d12';
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
-const wikiLinks = (value: string): string => {
-	return value.replace(/\[\[([^\]]*)\]\]/g, (_, r) => {
-		const [link, text] = r.includes(' | ') ? r.split(' | ') : [r, r];
-		const url = `/wiki/${link.replace(' ', '_')}`;
-		return `[${text}](${url})`;
-	});
-};
-
-const preProcess = (text: string): string => {
-	// look for liquid tags such as {% list "Cover", "order" %}, {% text "Telluric" %} or {% item "Blinded" %}
-	// and expand them according to how eleventy does it.
-	return text.replace(/{% (list|text|item) "([^"]+)"(, "([^"]+)")? %}/g, (_, tag, arg1, __, arg2) => {
-		switch (tag) {
-			case 'list': {
-				// {% list "Cover", "order" %} -> [Cover]( /wiki/Cover ) (ordered)
-				const listType = arg2 === 'order' ? '1.' : '-';
-				return `${listType} [${arg1}]( /wiki/${arg1.replace(' ', '_')} )`;
-			}
-			case 'text':
-				// {% text "Telluric" %} -> [Telluric]( /wiki/Telluric )
-				return `[${arg1}]( /wiki/${arg1.replace(' ', '_')} )`;
-			case 'item':
-				// {% item "Blinded" %} -> [Blinded]( /wiki/Blinded )
-				return `[${arg1}]( /wiki/${arg1.replace(' ', '_')} )`;
-			default:
-				return '';
-		}
-	});
-};
+const preProcessor = new MarkdownPreProcessor({
+	renderer: (tagName: string, ...args: string[]): string => {
+		return `{tag:${tagName}(${args.join(',')})}`;
+	},
+});
 
 interface RichTextProps {
 	children: string;
@@ -37,9 +15,10 @@ interface RichTextProps {
 }
 
 export const RichText: React.FC<RichTextProps> = ({ children, otherComponents }) => {
-	const preProcessed = wikiLinks(preProcess(children));
+	const preProcessed = preProcessor.process(children);
 	return (
 		<ReactMarkdown
+			remarkPlugins={[remarkGfm]}
 			components={{
 				p: ({ children }) => <p style={{ margin: 0 }}>{children}</p>,
 				...otherComponents,
